7a9be3b2b5baeee9562eb78ed29d72cbe4cdca97
Scott Causby <scottcausby@gmail.com>
2021-04-14 15:38:27 +1000

    classes, spread and rest

---
+++ classes.js
@@ -0,0 +1,50 @@
+// ES6
+
+class Human {
+    constructor() {
+        this.gender = 'male';
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = 'Scott';
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
+
+// ES7
+
+class Human {
+        gender = 'male';
+    
+
+    printGender = () => {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+        name = 'Scott';
+    
+
+    printMyName = () => {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
